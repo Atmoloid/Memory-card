2f3dde8144bbf4ea73e4a02a2e05bec7cd64e831
Lorenzo <lorenzopica31@gmail.com>
2025-06-18 16:07:49 +0200

    added card component and basic styling

---
+++ Memory-card/src/card.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './card.css'
+
+function Card() {
+ const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <div className='card-container'>
+
+     </div>
+    </>
+  )
+}
+
+export default Card
