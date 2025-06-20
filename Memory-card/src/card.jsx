af17d6fc1132176ff317839975204da7f40a86be
Lorenzo <lorenzopica31@gmail.com>
2025-06-20 16:35:01 +0200

    fetched first api

--- Memory-card/src/card.jsx
+++ Memory-card/src/card.jsx
@@ -1,16 +1,35 @@
-import { useState } from 'react'
-import './card.css'
+import { useState, useEffect } from 'react';
+import './card.css';
 
 function Card() {
- const [count, setCount] = useState(0)
+  const [pokemon, setPokemon] = useState(null);
+  const [count, setCount] = useState(1); // ID del Pokémon da visualizzare
+
+  useEffect(() => {
+    // Funzione per fetchare il Pokémon dalla PokéAPI
+    const fetchPokemon = async () => {
+      try {
+        const response = await fetch('https://pokeapi.co/api/v2/pokemon/${count}')
+        setPokemon(data);
+      } catch (error) {
+        console.error('Error while loading ', error);
+      }
+    };
+
+    fetchPokemon();
+  }, [count]); // Ricarica quando cambia il count
 
   return (
-    <>
-     <div className='card-container'>
-
-     </div>
-    </>
-  )
+    <div className='card-container'>
+      {pokemon ? (
+        <>
+          <img src={pokemon.sprites.front_default} alt={pokemon.name} />
+        </>
+      ) : (
+        <p>Caricamento...</p>
+      )}
+    </div>
+  );
 }
 
-export default Card
+export default Card;
